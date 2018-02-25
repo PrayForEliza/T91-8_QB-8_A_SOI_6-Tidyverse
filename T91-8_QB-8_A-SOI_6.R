3a44ca71b052cb6b9ef5b190139c962b0c371955
PrayForEliza <t.copelandjohnson@gmail.com>
2018-02-24 19:37:05 -0600

    Trying to change filepath to copy of raw data file that has been uploaded to Git repository.

--- T91-8_QB-8_A-SOI_6.R
+++ T91-8_QB-8_A-SOI_6.R
@@ -1,5 +1,5 @@
-df <- read.csv("C:/Users/tcopelan/Google Drive/BNL Summer 2017/43-648-TMCJ-17/Data/Energy-Dispersive X-Ray Spectroscopy/081117 Archive/T91-8_QB-8_A_SOI_6.csv",
-                    header = TRUE, sep = ",", quote = "\"", dec = ".", fill = TRUE, comment.char = "", skip = 9)
+df <- read.csv(text=getURL("https://github.com/PrayForEliza/T91-8_QB-8_A_SOI_6-Tidyverse/blob/master/T91-8_QB-8_A_SOI_6.csv"),
+                    header = T, skip = 10)
 df[] <- lapply(df,as.character)
 colnames(df) <- df[1,]
 df = df[-1, ]
