396d6c4be14fb2acaa7156b93c8b32dbbbaea4cd
PrayForEliza <t.copelandjohnson@gmail.com>
2018-03-04 20:08:38 -0600

    Added data visualization packages. Need to change variable types to <int>.

--- T91-8_QB-8_A-SOI_6.R
+++ T91-8_QB-8_A-SOI_6.R
@@ -1,5 +1,7 @@
 require(RCurl)
 require(yaml)
+require(ggplot2)
+require(dplyr)
 #Acquiring raw data----
 df <- read.csv(text=getURL("https://raw.githubusercontent.com/PrayForEliza/T91-8_QB-8_A_SOI_6-Tidyverse/master/T91-8_QB-8_A_SOI_6.csv"),
                     header = T, skip = 9) 
@@ -10,5 +12,5 @@ View(df)
 
 #Data Vizualization----
 
-
+#ggplot(df,aes(x="Weight%Total"))+geom_histogram(binwidth=5)
 
