6dbc504ae763ab942a417fe50e90a86ef40c2067
PrayForEliza <t.copelandjohnson@gmail.com>
2018-03-04 19:55:18 -0600

    Added code sections

--- T91-8_QB-8_A-SOI_6.R
+++ T91-8_QB-8_A-SOI_6.R
@@ -1,10 +1,14 @@
 require(RCurl)
 require(yaml)
+#Acquiring raw data----
 df <- read.csv(text=getURL("https://raw.githubusercontent.com/PrayForEliza/T91-8_QB-8_A_SOI_6-Tidyverse/master/T91-8_QB-8_A_SOI_6.csv"),
-                    header = T, skip = 9)
+                    header = T, skip = 9) 
 df[] <- lapply(df,as.character)
 colnames(df) <- df[1,]
 df = df[-1, ]
 View(df)
+
+#Data Vizualization----
+
 
 
