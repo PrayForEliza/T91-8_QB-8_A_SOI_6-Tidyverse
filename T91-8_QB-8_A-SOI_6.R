d233bcec6da1bbb924c3dcfd3ec40f0d9c9f8412
PrayForEliza <t.copelandjohnson@gmail.com>
2018-03-17 18:38:41 -0500

    FINALLY identified while histogram function wasn't working for df$'Weight%Total'.

--- T91-8_QB-8_A-SOI_6.R
+++ T91-8_QB-8_A-SOI_6.R
@@ -1,16 +1,19 @@
+#Required packages----
 require(RCurl)
 require(yaml)
 require(ggplot2)
 require(dplyr)
+require(magrittr)
 #Acquiring raw data----
 df <- read.csv(text=getURL("https://raw.githubusercontent.com/PrayForEliza/T91-8_QB-8_A_SOI_6-Tidyverse/master/T91-8_QB-8_A_SOI_6.csv"),
                     header = T, skip = 9) 
 df[] <- lapply(df,as.character)
 colnames(df) <- df[1,]
 df = df[-1, ]
-View(df)
-glimpse(df)
-#Data Vizualization----
+#View(df)
 
-#ggplot(df,aes(x="Weight%Total"))+geom_histogram(binwidth=5)
+#Data Vizualization----
+as_data_frame(df)
+df$`Weight%Total`=as.integer(as.character(df$`Weight%Total`))
+ggplot(df,aes(x=df$'Weight%Total'))+geom_histogram(binwidth = 5)
 
