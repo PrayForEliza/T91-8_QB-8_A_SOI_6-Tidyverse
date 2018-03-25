3d640f203927caaac450a92310424e0b473d596d
Copeland-Johnson <tcopelan@iastate.edu>
2018-03-25 17:55:14 -0500

    Extract data from approved location.

--- T91-8_QB-8_A-SOI_6.R
+++ T91-8_QB-8_A-SOI_6.R
@@ -4,12 +4,15 @@ require(yaml)
 require(ggplot2)
 require(dplyr)
 require(magrittr)
+require(googlesheets)
 #Acquiring raw data----
-df <- read.csv(text=getURL("https://raw.githubusercontent.com/PrayForEliza/T91-8_QB-8_A_SOI_6-Tidyverse/master/T91-8_QB-8_A_SOI_6.csv"),
-                    header = T, skip = 9) 
-df[] <- lapply(df,as.character)
-colnames(df) <- df[1,]
-df = df[-1, ]
+##df <- read.csv(text=getURL("https://raw.githubusercontent.com/PrayForEliza/T91-8_QB-8_A_SOI_6-Tidyverse/master/T91-8_QB-8_A_SOI_6.csv"),
+##                    header = T, skip = 9) 
+input <- gs_title("T91-8_QB-8_A_SOI_6.csv")
+df <- gs_read_csv(input, header = T, skip =10)
+#df[] <- lapply(df,as.character)
+#colnames(df) <- df[1,]
+#df = df[-1, ]
 #View(df)
 
 #Data Vizualization----
